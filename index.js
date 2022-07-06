f63c5187f65a36b8e658436fbc628d38440db8a0
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2022-07-06 19:11:04 -0300

    in

--- index.js
+++ index.js
@@ -21,85 +21,60 @@ app.use(express.static('public'))//ponte para os arquivos estaticos
 
 
 //index
-app.get('/', (req,res)=>{
-    res.render('index')
-})// rota principal
+ app.get('/', (req,res)=>{
+     res.render('index')
+ }) //rota principal
 
-//entrar
-app.get('/entrar', (req, res) => {
-    res.render('entrar')
-})
+ //entrar
+ app.get('/entrar', (req, res) => {
+     res.render('entrar')
+ })
 
-//criar login
-app.get('/login', (req, res) => {
-    res.render('login')
-})
-//home
-app.get('/home', (req, res) => {
-    res.render('home')
-})
+ //criar login
+ app.get('/login', (req, res) => {
+     res.render('login')
+ })
+ //home
+ app.get('/home', (req, res) => {
+     res.render('home')
+ })
 
-//gato diario
-app.get('/gastos', (req, res) => {
-    res.render('gastos')
-})
+ //gato diario
+ app.get('/gastos', (req, res) => {
+     res.render('gastos')
+ })
 
-//contatos
-app.get('/contato', (req, res) => {
-    res.render('contato')
-})
+ //contatos
+ app.get('/contato', (req, res) => {
+     res.render('contato')
+ })
 
-//sobre
-app.get('/sobre', (req, res) => {
-    res.render('intro')
-})
+ //sobre
+ app.get('/sobre', (req, res) => {
+     res.render('intro')
+ })
 
-//teste
-app.get('/teste', (req, res) => {
-    res.render('teste')
-})
 
 //-------fim das rotas-----------
 
-
-//INSERINDO DADOS
+//resgatento os dados
 app.post('/gastos/insertgastos', (req,res)=>{
 
-    const valorGasto = req.body.valorGasto
-    const item = req.body.item
+    const item = req.body.title
+    const valorGasto = req.body.pageqty
 
-    const sql = `INSERT INTO gastos (valorGasto, item) VALUES ('${valorGasto}','${item}')`//codigo que ira para o banco de dados 
+    const sql = `INSERT INTO gastos (item, valorGasto) VALUES ('${item}','${valorGasto}')`//codigo que ira para o banco de dados 
 
     conn.query(sql, function(err){
         if(err){
             console.log(err)
             return
         }
-        res.redirect('/gastos')
+        res.redirect('/')
     }) //executando o codigo sql
+
 })
 
-
-//resgatento os dados
-app.get('/home', (req, res) => {
-    const sql = "SELECT * FROM gastos"
-
-    conn.query(sql, function (err, data){
-        if(err){
-            console.log(err)
-            return
-        }
-
-        const gastos = data
-        
-        console.log(gastos)
-
-        res.render('home', {gastos})
-    })
-})
-
-//teste
-//resgatento os dados
 app.get('/teste', (req, res) => {
     const sql = "SELECT * FROM gastos"
 
@@ -110,21 +85,31 @@ app.get('/teste', (req, res) => {
         }
 
         const gastos = data
+        res.render('teste', {gastos})
+    })
+})
 
-        console.log(gastos)
+app.get('/testegastos', (req, res) => {
+    const sql = "SELECT * FROM gastos"
 
-        res.render('gastos', {gastos})
+    conn.query(sql, function (err, data){
+        if(err){
+            console.log(err)
+            return
+        }
+
+        const gastos = data
+        res.render('testegastos', {gastos})
     })
 })
 
 
 
 
-////
 const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password:'',
     database:'desafogando',
     port: '3307'
 })// dados do banco do dados 

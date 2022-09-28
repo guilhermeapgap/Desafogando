bf54e4a44a28d66d95b6ac0714aee58b2c618ef4
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2022-09-28 17:21:49 -0300

    mudanças a serem feitas

--- index.js
+++ index.js
@@ -1,152 +1,146 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const mysql = require('mysql')
-const app = express()
+// const express = require('express')
+// const exphbs = require('express-handlebars')
+// const mysql = require('mysql')
+// const app = express()
 
 
-app.use(
-    express.urlencoded({
-        extended:true,
-}),
-)
+// app.use(
+//     express.urlencoded({
+//         extended:true,
+// }),
+// )
 
-app.use(express.json())//pegando o body em json
+// app.use(express.json())//pegando o body em json
 
-app.engine('handlebars', exphbs.engine())
-app.set('view engine','handlebars')//template engine
+// app.engine('handlebars', exphbs.engine())
+// app.set('view engine','handlebars')//template engine
 
-app.use(express.static('public'))//ponte para os arquivos estaticos
+// app.use(express.static('public'))//ponte para os arquivos estaticos
 
-// ----------rotas-------------
-
-
-//index
- app.get('/', (req,res)=>{
-     res.render('index')
- }) //rota principal
-
- //entrar
- app.get('/entrar', (req, res) => {
-     res.render('entrar')
- })
-
- //criar login
- app.get('/login', (req, res) => {
-     res.render('login')
- })
- //home
- app.get('/home', (req, res) => {
-     res.render('home')
- })
-
- //gato diario
- app.get('/gastos', (req, res) => {
-     res.render('gastos')
- })
-
- //contatos
- app.get('/contato', (req, res) => {
-     res.render('contato')
- })
-
- //sobre
- app.get('/sobre', (req, res) => {
-     res.render('intro')
- })
+// // ----------rotas-------------
 
 
-//-------fim das rotas-----------
+// //index
+//  app.get('/', (req,res)=>{
+//      res.render('index')
+//  }) //rota principal
 
-//resgatento os dados
-app.post('/gastos/insertgastos', (req,res)=>{
+//  //entrar
+//  app.get('/entrar', (req, res) => {
+//      res.render('entrar')
+//  })
 
-    const item = req.body.title
-    const valorGasto = req.body.pageqty
+//  //criar login
+//  app.get('/login', (req, res) => {
+//      res.render('login')
+//  })
+//  //home
+//  app.get('/home', (req, res) => {
+//      res.render('home')
+//  })
 
-    const sql = `INSERT INTO gastos (item, valorGasto) VALUES ('${item}','${valorGasto}')`//codigo que ira para o banco de dados 
+//  //gato diario
+//  app.get('/gastos', (req, res) => {
+//      res.render('gastos')
+//  })
 
-    conn.query(sql, function(err){
-        if(err){
-            console.log(err)
-            return
-        }
-        res.redirect('/testegastos')
-    }) //executando o codigo sql
+//  //contatos
+//  app.get('/contato', (req, res) => {
+//      res.render('contato')
+//  })
 
-})
-
-
-
-app.get('/teste', (req, res) => {
-    const sql = "SELECT * FROM gastos"
-
-    conn.query(sql, function (err, data){
-        if(err){
-            console.log(err)
-            return
-        }
-
-        const gastos = data
-        res.render('teste', {gastos})
-    })
-})
-
-app.get('/testegastos', (req, res) => {
-    const sql = "SELECT * FROM gastos "
-    const somadb = `SELECT SUM (valorGasto) as total FROM gastos`
-
-    conn.query(sql, function (err, data){
-        if(err){
-            console.log(err)
-            return
-        }
-
-        const gastos = data
-        //console.log(gastos)
-        
-        res.render('testegastos', {gastos})
-        
-    })
-    
-})
-
-
-
-
-//  app.get('/testegastos', (req, res) => {
-//      const somadb = `SELECT SUM (valorGasto) as total FROM gastos`
-    
-//      conn.query(somadb, function (err, data){
-//          if(err){
-//              console.log(err)
-//              return
-//          }
-
-//          const db = data
-//          res.render('testegastos', {db})
-//          console.log(somadb)
-//      })
+//  //sobre
+//  app.get('/sobre', (req, res) => {
+//      res.render('intro')
 //  })
 
 
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password:'',
-    database:'desafogando',
-    port: '3307'
-})// dados do banco do dados 
+// //-------fim das rotas-----------
 
-conn.connect(function(err){
-    if(err){
-        console.log(err)
-        return
-    }
+// //resgatento os dados
+// app.post('/gastos/insertgastos', (req,res)=>{
 
-    console.log('conectou ao MySQL!!!')
+//     const item = req.body.title
+//     const valorGasto = req.body.pageqty
 
+//     const sql = `INSERT INTO gastos (item, valorGasto) VALUES ('${item}','${valorGasto}')`//codigo que ira para o banco de dados 
+
+//     conn.query(sql, function(err){
+//         if(err){
+//             console.log(err)
+//             return
+//         }
+//         res.redirect('/testegastos')
+//     }) //executando o codigo sql
+
+// })
+
+
+
+// app.get('/teste', (req, res) => {
+//     const sql = "SELECT * FROM gastos"
+
+//     conn.query(sql, function (err, data){
+//         if(err){
+//             console.log(err)
+//             return
+//         }
+
+//         const gastos = data
+//         res.render('teste', {gastos})
+//     })
+// })
+
+// app.get('/testegastos', (req, res) => {
+//     const sql = "SELECT * FROM gastos "
+//     const somadb = `SELECT SUM (valorGasto) as total FROM gastos`
+
+//     conn.query(sql, function (err, data){
+//         if(err){
+//             console.log(err)
+//             return
+//         }
+
+//         const gastos = data
+//         //console.log(gastos)
+        
+//         res.render('testegastos', {gastos})
+        
+//     })
+    
+// })
+
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+const conn = require('./db/conn')
+const Task = require('./models/Task')
+const tasksRoutes = require('./routes/TaskRouters')
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.use('/tasks', tasksRoutes)
+
+conn
+.sync()
+.then(()=>{
     app.listen(5000)
-}) //criando uma conexãos
+})
+.catch((err)=> console.log(err))
+
 
  
 
